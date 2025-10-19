fc67027b91bdd31de734b336491d8eb577a34200
olaayyad4 <olaayyad4@gmail.com>
2025-10-19 09:51:00 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("form"). onsubmit = function () {
+  alert ("submitted");  return false; 
+};
+
+document.getElementById("tr"). onclick = function () {
+  alert ("submitted");  return false; 
+};
+
